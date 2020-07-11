b33d2770b89ad4e311563e9b63c0f2f3153267bc
Yadigar Muradov <yadigar-m@hotmail.com>
2020-07-11 16:02:15 +0200

    200711 1602: Created RelayCommand.

---
+++ zzz_MVVM_WPF_Multiple_Ms_Vs_VMs/Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using System.Windows.Input;
+
+namespace zzz_MVVM_WPF_Multiple_Ms_Vs_VMs.Commands
+{
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action DoWork; // Delegate. 
+
+        // Constructor. 
+        public RelayCommand(Action work)
+        {
+            DoWork = work;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //throw new NotImplementedException();
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //throw new NotImplementedException();
+
+            DoWork();
+        }
+    }
+}
